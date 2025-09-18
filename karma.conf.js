905d0d67418e182ebb3929009b2e2ba1c52dd3ba
Ameth Ba <amethba8826@gmail.com>
2025-09-18 15:28:24 +0000

    Configure Karma pour CI avec Puppeteer

--- karma.conf.js
+++ karma.conf.js
@@ -1,34 +1,40 @@
-module.exports = function (config) {
-    process.env.CHROME_BIN = require('puppeteer').executablePath();
+// karma.conf.js
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 
-    config.set({
-        basePath: '',
-        frameworks: ['jasmine', '@angular-devkit/build-angular'],
-        plugins: [
-            require('karma-jasmine'),
-            require('karma-chrome-launcher'),
-            require('karma-jasmine-html-reporter'),
-            require('karma-coverage'),
-            require('@angular-devkit/build-angular/plugins/karma')
-        ],
-        client: {
-            clearContext: false
-        },
-        coverageReporter: {
-            dir: require('path').join(__dirname, './coverage/omni365-showcase'),
-            subdir: '.',
-            reporters: [
-                { type: 'html' },
-                { type: 'text-summary' }
-            ]
-        },
-        reporters: ['progress', 'kjhtml'],
-        port: 9876,
-        colors: true,
-        logLevel: config.LOG_INFO,
-        autoWatch: true,
-        browsers: ['ChromeHeadless'],
-        singleRun: true,
-        restartOnFileChange: true
-    });
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/omni365-showcase'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,          // important for CI
+    singleRun: true,           // runs tests once
+    restartOnFileChange: false,
+    browsers: ['ChromeHeadless'],
+    browserNoActivityTimeout: 60000, // 60s pour éviter timeout sur CI lent
+  });
 };
